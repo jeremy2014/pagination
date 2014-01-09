93b0e9b6155faed216db7a2960367c2df7af31e5
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2014-01-09 12:38:25 +0800

    Pagination using Kaminari gem

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+paginates_per 2
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pagination::Application.config.secret_key_base = '397e2e148809ffdb5d3b28cc3116ffc6a4591dde8e54c294d9406d0c8e7cab6339d29d5d52401112034d69727e2d5c67e22425b749bad5f7c930b631e9235bdd'
